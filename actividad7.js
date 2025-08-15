94b13a1376259d7baaa1a42d78b3a247d2e8b3dd
Marcos Valladares <markosvalla@hotmail.com>
2025-08-14 23:37:09 -0300

    cambie la actividad 7

--- actividad7.js
+++ actividad7.js
@@ -1,7 +1,7 @@
 let numero = +prompt('Ingresa un numero (NO mayor que 50)')
 let resultado = ""
 
-if (numero <= +50) {
+if (numero <= 50) {
     for (let i = numero; i <= numero ; i--) {
         resultado = i
         console.log(String(resultado).repeat(i))
