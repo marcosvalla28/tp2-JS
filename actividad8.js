a8c96fa72808d679d673b532f85cb2a6a850402c
Marcos Valladares <markosvalla@hotmail.com>
2025-08-14 23:19:33 -0300

    cambien un valor en actividad 8

--- actividad8.js
+++ actividad8.js
@@ -2,7 +2,7 @@ let numero = +prompt("Ingresa un numero (no mayor que 50)")
 let resultado = ""
 
 if (numero <= 50) {
-    for (let i = 130; i <= numero ;i++) {
+    for (let i = 0; i <= numero ;i++) {
     resultado += i 
     console.log(resultado)
 }
