70067e1a9d04ad29ce8963d14551ed84cf505a2c
Marcos Valladares <markosvalla@hotmail.com>
2025-08-14 23:29:55 -0300

    cambie un 0 por un 1

--- actividad8.js
+++ actividad8.js
@@ -2,7 +2,7 @@ let numero = +prompt("Ingresa un numero (no mayor que 50)")
 let resultado = ""
 
 if (numero <= 50) {
-    for (let i = 0; i <= numero ;i++) {
+    for (let i = 1; i <= numero ;i++) {
     resultado += i 
     console.log(resultado)
 }
